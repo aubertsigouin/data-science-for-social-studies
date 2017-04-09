963728c686e543bdc983402c9354aad3b4f8187e
aubertsigouin <sigouin-lebel.aubert@courrier.uqam.ca>
2017-04-09 01:57:59 -0400

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,24 @@
+>>> print "Suite de Fibonacci :"
+>>> a, b, c = 1, 1, 1
+>>> print 1
+1
+>>> while c<15:
+	a, b, c = b, a+b, c+1
+	print b
+
+	
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+>>> 
